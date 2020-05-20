ab9393d311a1ae9e94b51f0a75edcd58641b60f9
diego <diegooliveira.tsi@gmail.com>
2020-05-20 11:12:24 -0300

    feat: rewrite appintment repository to not depend of TYPEORM

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,7 @@
 import Appointment from '../infra/typeorm/entities/Appointment'
-
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO'
 interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>
   findByDate(date: Date): Promise<Appointment | undefined>
 }
 
