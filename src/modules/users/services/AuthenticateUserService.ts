941dc6b65bf3ad2cfd36b744bd5b185507f0444c
diego <diegooliveira.tsi@gmail.com>
2020-05-20 11:44:38 -0300

    refactor: separete users repository from infra repository

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -5,23 +5,23 @@ import { sign, verify } from 'jsonwebtoken'
 import authConfig from '@config/auth'
 import AppError from '@shared/errors/AppError'
 
+import IUsersRepository from '../repositories/IUserRepository'
 import User from '../infra/typeorm/entities/User'
 
-interface Request {
+interface IRequest {
   email: string
   password: string
 }
 
-interface Response {
+interface IResponse {
   user: User
   token: string
 }
 
 class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<Response> {
-    const usersRepository = getRepository(User)
-
-    const user = await usersRepository.findOne({ where: { email } })
+  constructor(private usersRepository: IUsersRepository) {}
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email)
 
     if (!user) {
       throw new AppError('Incorrect email/password combination', 401)
