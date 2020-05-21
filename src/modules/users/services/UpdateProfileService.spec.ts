7b924a4ef4d93d6252daeb711e4c1e6c67415716
diego <diegooliveira.tsi@gmail.com>
2020-05-21 15:52:28 -0300

    feat: update profile and show user infos

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -36,6 +36,16 @@ describe('UpdateProfile', () => {
     expect(updatedUser.email).toBe('johntre@gmail.com')
   })
 
+  it('should not be able to update the profile from non-existing-user', async () => {
+    await expect(
+      updateProfile.execute({
+        email: 'non-existing-user',
+        name: 'test',
+        user_id: 'test',
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
   it('should not be able to change to another user email', async () => {
     await fakeUsersRepository.create({
       name: 'John Doe',
