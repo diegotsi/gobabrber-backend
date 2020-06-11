4a320a6039c70c7b36bd5585d9283210a385c2ee
diego <diegooliveira.tsi@gmail.com>
2020-06-11 07:23:56 -0300

    feat: using celebrate to validate recevived data

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 
 import express, { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
+import { errors } from 'celebrate'
 import 'express-async-errors'
 
 import routes from './routes'
@@ -19,6 +20,8 @@ app.use(express.json())
 app.use('/files', express.static(uploadConfig.tmpFolder))
 app.use(routes)
 
+app.use(errors())
+
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
@@ -26,7 +29,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
       message: err.message,
     })
   }
-
+  console.log(err)
   return response.status(500).json({
     status: 'error',
     message: 'Internal server error',
